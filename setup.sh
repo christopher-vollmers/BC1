8e7e51f8631615d71a98ae8946b57ec2a3b1ed3a
Vollmers Lab <ig88@holowan.ucsc.edu>
2023-08-13 22:13:51 -0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cwd=$(pwd)
+
+# Resolve dependencies
+
+echo 'Pip installables (scipy, numpy, mappy, Cython)'
+python3 -m pip install --user --upgrade scipy numpy mappy Cython editdistance
+
+echo 'Racon'
+git clone --recursive https://github.com/isovic/racon.git racon
+cd racon
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd $cwd
+
+echo 'abpoa'
+wget https://github.com/yangao07/abPOA/releases/download/v1.4.1/abPOA-v1.4.1.tar.gz
+tar -zxvf abPOA-v1.4.1.tar.gz
+cd abPOA-v1.4.1; make
+cd $cwd
+rm abPOA-v1.4.1.tar.gz
+
+echo 'Medaka'
+
+pip install medaka
+
+echo 'Done'
