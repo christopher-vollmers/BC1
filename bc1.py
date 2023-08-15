19f72cdb171c1eb7512ef26980aca821a23e5c31
Vollmers Lab <ig88@holowan.ucsc.edu>
2023-08-14 21:04:44 -0700

    updated help message

--- bc1.py
+++ bc1.py
@@ -19,17 +19,17 @@ from utils import delegating_consensus_generation, process_batch, determine_cons
 
 
 parser=argparse.ArgumentParser()
-parser.add_argument('-i','--input_fasta',type=str)
-parser.add_argument('-o','--output_file_root',type=str)
-parser.add_argument('-s','--subread_files',type=str)
-parser.add_argument('-m','--medaka',action='store_true',default=False)
-parser.add_argument('-f','--fuzzy',action='store_true',default=False)
-parser.add_argument('-S','--STARsolo_sam_file',type=str)
-parser.add_argument('-b','--subsample',type=int,default=1000)
+parser.add_argument('-i','--input_fasta',type=str, help='demultiplexed,trimmed, and reoriented R2C2 consensus reads as output by C3POa_postprocessing.py')
+parser.add_argument('-o','--output_file_root',type=str, help='root file name. Intermediate and final output files will be start with this')
+parser.add_argument('-s','--subread_files',type=str, help='R2C2 subreads as output by C3POa. Can be multiple comma separated files')
+parser.add_argument('-m','--medaka',action='store_true',default=False, help='if set, medaka will be run. required for highest accuracy but definitely slows things down. assumes data was produced with R10.4 pores at 400bp/s speed')
+parser.add_argument('-f','--fuzzy',action='store_true',default=False, help='if set, reads with a single mismatch between their UMIs will be combined. By default, only reads with identical UMIs will be combined.')
+parser.add_argument('-S','--STARsolo_sam_file',type=str, help='experimental setting.Probably best left alone for now')
+parser.add_argument('-b','--subsample',type=int,default=1000,help='subreads will be subsampled to this number.' )
 parser.add_argument('-t', '--threads', type=int, help='defines the number of threads the multiprocessing will use')
 parser.add_argument('-u','--UMIpatterns',type=str, help="""UMI patterns separated by commas. \nAn example would be '5.0:3.GACAG.NNNNNNNNNNNNNN.,3.0:3.CAC.NNNNNN.TTTT' that would indicate two UMIs.
                                                          The first at the 5prime end of the read starting somewhere in the first 3 bases of the read and is flanked on the left with 'GACAG' and is 14nt long.
-                                                         The second at the 3prime end of the read starting somewhere in the first 3 bases of the (reverse complemented) read and is flanked on the left with 'CAC' and on the right with 'TTTT' and is 6nt long""")
+                                                         The second at the 3prime end of the read starting somewhere in the first 3 bases of the (reverse complemented) read and is flanked on the left with 'CAC' and on the right with 'TTTT' and is 6nt long. IUPAC wild card bares can be used at any position""")
 
 
 
