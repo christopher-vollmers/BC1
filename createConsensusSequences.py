6fa7148da240ba537fbc1074979d4d031ead0e44
Vollmers Lab <ig88@holowan.ucsc.edu>
2024-01-18 11:21:04 -0800

    added minimap2 path

--- createConsensusSequences.py
+++ createConsensusSequences.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath(PATH))
 BD1Path = '/'.join(os.path.realpath(__file__).split('/')[:-1]) + '/'
 abpoa=BD1Path+'/abPOA-v1.4.1/bin/abpoa'
 racon=BD1Path+'/racon/build/bin/racon'
-
+minimap2=BD1Path+'/minimap2/minimap2'
 
 from utils import process_batch
 
@@ -54,7 +54,7 @@ def main():
         elif type1=='sub':
             subreads[UMInumber].add((name,seq,qual))
 
-    processed,singles=process_batch(subreads,reads,processed,singles,subsample,medaka,threads,out,combined_UMI_length,abpoa,racon,temp_folder)
+    processed,singles=process_batch(subreads,reads,processed,singles,subsample,medaka,threads,out,combined_UMI_length,abpoa,racon,minimap2,temp_folder)
     out.close()
     print(f'{processed} reads merged from multi-read UMIs, {singles} reads not merged because incomplete or single-read UMI', ' '*60)
 
